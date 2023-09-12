cbc5a789d9941671abeb3e2c31ffd2519ea41331
Emilia Alanen <alanemi@digitalentshelsinki.internal>
2023-09-12 13:10:47 +0300

    3.5 done

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let persons = [
   {
     "name": "Arto Hellas",
@@ -55,6 +57,14 @@ app.delete('/api/persons/:id', (req, res) => {
   persons = persons.filter(person => person.id !== id)
 
   res.status(204).end()
+})
+
+app.post('/api/persons', (req, res) => {
+  const person = req.body
+  person.id = Math.floor(Math.random()*1000000)
+
+  persons = persons.concat(person)
+  res.json(person)
 })
 
 const PORT = 3001
